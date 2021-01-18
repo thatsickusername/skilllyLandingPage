47094d19d2c774c3f5e9fe560e51f10209925cfe
thatsickusername <60377461+thatsickusername@users.noreply.github.com>
2021-01-18 06:14:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-var hamburger = document.querySelector(".hamburger");
-	var menu = document.querySelector(".menu");
+gsap.from('.hero-text', {
+	opacity: 0, 
+	duration: 2, 
+	y: -50
+})
 
-	hamburger.addEventListener("click", function(){
-		menu.classList.toggle("active");
-	})
+gsap.from('.boxes', {
+	scrollTrigger: '.boxes',
+	opacity: 0,
+	y: 50,
+	duration: 0.6,
+	scale: 0.9
+})
+
+gsap.from('.midwayTextContainer h2', {
+	scrollTrigger: '.midwayTextContainer',
+	opacity: 0,
+	y: 80,
+	duration: 1,
+})
+
+gsap.from('.midwayTextContainer h4', {
+	scrollTrigger: '.midwayTextContainer',
+	opacity: 0,
+	y: 50,
+	duration: 1.2,
+})
+
+gsap.from('.odd', {
+	scrollTrigger: '.odd',
+	opacity: 0,
+	x: -100,
+	duration: 0.7,
+	scale: 0.9,
+	stagger: 1
+})
+
+gsap.from('.even', {
+	scrollTrigger: '.even',
+	opacity: 0,
+	x: 100,
+	duration: 0.7,
+	scale: 0.9,
+	stagger: 1
+})
+
+
